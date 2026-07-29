ac73b91c81fb70a5c4a534b44f10d7426a1fa592
Fernando García <lfgc851@gmail.com>
2026-07-28 21:19:48 -0600

    MongoDB from docker config setup

--- src/WeatherService.Api/Program.cs
+++ src/WeatherService.Api/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using WeatherService.Api.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// register mongo config
+builder.Services.Configure<MongoSettings>(builder.Configuration.GetSection("MongoSettings"));
+
+builder.Services.AddSingleton<IMongoDatabase>(sp =>
+{
+    var settings = sp.GetRequiredService<IOptions<MongoSettings>>().Value;
+    var client = new MongoClient(settings.ConnectionString);
+    return client.GetDatabase(settings.DatabaseName);
+});
 
 var app = builder.Build();
 
