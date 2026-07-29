cc32fe98151a18f7bc39d82c5abaf7e03dab2f5d
Fernando García <lfgc851@gmail.com>
2026-07-28 22:43:49 -0600

    End point controller first implementation

---
+++ src/WeatherService.Api/Controllers/WeatherController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherService.Api.DTOs;
+using WeatherService.Api.Services;
+
+namespace WeatherService.Api.Controllers;
+
+[ApiController]
+[Route("api/weather")]
+public class WeatherController : ControllerBase
+{
+    private readonly IWeatherService _weatherService;
+
+    public WeatherController(IWeatherService weatherService)
+    {
+        _weatherService = weatherService;
+    }
+
+    [HttpGet("coordinates")]
+    public async Task<ActionResult<WeatherResponseDto>> GetByCoordinates([FromQuery] WeatherQueryDto query)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var result = await _weatherService.GetWeatherAsync(query.Latitude, query.Longitude);
+        return Ok(result);
+    }
+}
---
+++ src/WeatherService.Api/Services/IWeatherService.cs
@@ -0,0 +1,8 @@
+﻿using WeatherService.Api.DTOs;
+
+namespace WeatherService.Api.Services;
+
+public interface IWeatherService
+{
+    Task<WeatherResponseDto> GetWeatherAsync(double latitude, double longitude);
+}
